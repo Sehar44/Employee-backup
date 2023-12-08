9bb7b05e118219ad951035049c35f7cde985026a
Sehar Taj <sehartaj44@gmail.com>
2023-12-08 13:07:41 +0530

    Adding files

---
+++ server/api.js
@@ -0,0 +1,39 @@
+// api.js
+const express = require('express');
+const userModel = require('./userModel'); // Import the model schema
+
+const router = express.Router();
+
+// Read
+router.get("/api/employee", async (req, res) => {
+    const data = await userModel.find({});
+    res.json({ success: true, data: data });
+});
+
+// Create data
+router.post("/api/employee", async (req, res) => {
+    console.log(req.body);
+    const data = new userModel(req.body);
+    await data.save();
+    res.send({ success: true, message: "data saved successfully", data: data });
+});
+
+// Update
+router.put("/api/employee", async (req, res) => {
+    console.log(req.body);
+    const { _id, ...rest } = req.body;
+    console.log(rest);
+    const data = await userModel.updateOne({ _id: _id }, rest);
+    res.send({ success: true, message: "data updated successfully", data: data });
+});
+
+// Delete
+router.delete("/api/employee/:id", async (req, res) => {
+    const id = req.params.id;
+    console.log(id);
+    const data = await userModel.deleteOne({ _id: id });
+    res.send({ success: true, message: "data deleted successfully", data: data });
+});
+
+module.exports = router;
+
---
+++ server/index.js
@@ -0,0 +1,31 @@
+// index.js
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const apiRouter = require('./api'); // Import the API endpoints
+const path= require('path');
+
+const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost:27017/crudoperation';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, './web')));
+
+const PORT = process.env.PORT || 3000;
+
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("connected to db");
+        app.listen(PORT, '0.0.0.0', () => console.log("Server is running"));
+    })
+    .catch((err) => console.log(err));
+
+app.use('/', apiRouter); // Use the API routes from api.js
+
+// Serve the React app for any other routes
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build','index.html'));
+});
+
